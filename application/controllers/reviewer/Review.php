64728cbc7a2c9304660f22613f20c9edc09e78c1
Asif Zinda Nihriroh <33834177+asifzinda@users.noreply.github.com>
2018-06-25 22:10:32 +0700

    Add files via upload

--- application/controllers/reviewer/Review.php
+++ application/controllers/reviewer/Review.php
@@ -4,11 +4,34 @@ class Review extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
+		$this->load->helper(array('form','url'));
+		$this->load->model('reviewer/Review_M');
 	}
 
 	public function index(){
-		$this->load->view("reviewer/reviewview");
+		$x = $this->Review_M->tampil()->result();
+		$data['review_judul'] = $x;
+		$this->load->view('reviewer/ReviewView' ,$data);
 	}
-
 	
+	public function editTampil(){
+		$id = $this->uri->segment(4);
+		$x = $this->Review_M->get_tampil($id)->row_array();
+		//$data['review_judul'] = $x;
+		$data =array(
+			'id'=>$id,
+			'x' => $x
+		);
+		$this->load->view('reviewer/editReviewView' ,$data);
+	}
+	
+    public function save_editTampil(){
+        $id = $this ->input->post('id');
+        $data = array(
+            'saran' => $this ->input ->post('saran'),
+			'keterangan' => $this ->input ->post('keterangan'));
+        $this ->db ->where('id',$id);
+        $this ->db ->update('review_judul',$data);
+        redirect('reviewer/Review');
+    }
 }
