589b8e7372640209ae4fd684896dd014d35f3832
Riska Marita Lisviana <33944485+riskamarita@users.noreply.github.com>
2018-06-25 13:18:29 +0700

    Grafik

--- application/views/mahasiswa/Template.php
+++ application/views/mahasiswa/Template.php
@@ -76,29 +76,80 @@
 	    <div class="container">
 	    	
 	     <body
-		  <form>
-
-			
-			</select></td></tr>
-			<br></br>
-			<tr><td><input type="submit" name="kirim" value="OK"/>
-			
-			</td></tr>
-		</body>
+		 
+    <head>
+        <title></title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.1/Chart.min.js"></script>
+    </head>
+    <body>
+        
+        <div class="span6">
+	      		
+	      		<div class="widget">
+							
+					<div class="widget-header">
+						<i class="icon-list-alt"></i>
+						<h3>Grafik Usulan Judul</h3>
+					</div> <!-- /widget-header -->
+					
+					<div class="widget-content">
+						 <canvas id="myChart" width="600" height="400"></canvas>
+<script>
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Sisem Informasi", "Game", "Android", "AI", "E-commers", "Multimedia"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
+					</div> <!-- /widget-content -->
+				
+				</div> <!-- /widget -->
+									
+		      </div> <!-- /span6 -->
 	      	
-	      
-	    </div> <!-- /container -->
-	    
-	</div> <!-- /main-inner -->
-    
-</div> <!-- /main -->
+	      </div> <!-- /row -->
+     
    
 
+     
 
 <script src="<?php echo base_url().'assets/js/jquery-2.2.4.min.js'?>"></script>
 <script src="<?php echo base_url().'assets/js/bootstrap.js'?>"></script>
 <script src="<?php echo base_url().'assets/js/jquery.dataTables.min.js'?>"></script>
 <script src="<?php echo base_url().'assets/js/moment.js'?>"></script>
+
 <script>
 	$(document).ready(function(){
 		$('#mydata').DataTable();
@@ -124,53 +175,8 @@
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 </head>
-<!-- /main -->
-<div class="extra">
-  <div class="extra-inner">
-    <div class="container">
-      <div class="row">
-                    <div class="span3">
-                        <h4>
-                            About Free Admin Template</h4>
-                        <ul>
-                            <li><a href="javascript:;">EGrappler.com</a></li>
-                            <li><a href="javascript:;">Web Development Resources</a></li>
-                            <li><a href="javascript:;">Responsive HTML5 Portfolio Templates</a></li>
-                            <li><a href="javascript:;">Free Resources and Scripts</a></li>
-                        </ul>
-                    </div>
-                    <!-- /span3 -->
-                    <div class="span3">
-                        <h4>
-                            Support</h4>
-                        <ul>
-                            <li><a href="javascript:;">Frequently Asked Questions</a></li>
-                            <li><a href="javascript:;">Ask a Question</a></li>
-                            <li><a href="javascript:;">Video Tutorial</a></li>
-                            <li><a href="javascript:;">Feedback</a></li>
-                        </ul>
-                    </div>
-                    <!-- /span3 -->
-                    <div class="span3">
-                        <h4>
-                            Something Legal</h4>
-                        <ul>
-                            <li><a href="javascript:;">Read License</a></li>
-                            <li><a href="javascript:;">Terms of Use</a></li>
-                            <li><a href="javascript:;">Privacy Policy</a></li>
-                        </ul>
-                    </div>
-                    <!-- /span3 -->
-                    <div class="span3">
-                        <h4>
-                            Open Source jQuery Plugins</h4>
-                        <ul>
-                            <li><a href="http://www.egrappler.com">Open Source jQuery Plugins</a></li>
-                            <li><a href="http://www.egrappler.com;">HTML5 Responsive Tempaltes</a></li>
-                            <li><a href="http://www.egrappler.com;">Free Contact Form Plugin</a></li>
-                            <li><a href="http://www.egrappler.com;">Flat UI PSD</a></li>
-                        </ul>
-                    </div>
+
+
                     <!-- /span3 -->
                 </div>
       <!-- /row --> 
